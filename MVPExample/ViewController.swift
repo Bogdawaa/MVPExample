ecb8968208a734920fb174bb767e86d383f4aba7
Bogdan Fartdinov <bogdan6246@yandex.ru>
2022-08-04 17:11:25 +0300

    Initial Commit

---
+++ MVPExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPExample
+//
+//  Created by Bogdan Fartdinov on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
